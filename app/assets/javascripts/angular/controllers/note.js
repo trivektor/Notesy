181d2945f83c865b48753d41f8aa690b9db1f395
trivektor <trivektor@gmail.com>
2013-02-10 00:11:56 -0800

    use indexedDB to store notes

--- app/assets/javascripts/angular/controllers/note.js
+++ app/assets/javascripts/angular/controllers/note.js
@@ -1,14 +1,43 @@
 function NoteController($scope) {
   $scope.notes = [];
 
-  $scope.addNote = function() {
-    $scope.notes.push({
-      description : $scope.noteDescription,
-      createdAt   : moment().format('MMMM Do YYYY, h:mm:ss a')
-    })
-    $scope.noteDescription = '';
+  var getAllFromDB = function() {
+    $scope.store.getAll(
+      /* onSuccess */
+      function(/* array */ data) {
+        $scope.$apply(function() {
+          $scope.notes = data;
+        })
+      },
+      /* onError */
+      function(error) {
+        console.log(error);
+      }
+    )
   }
-  
+
+  $scope.store = new IDBStore({
+    storeName: 'Notesy1',
+    storePrefix: 'IDBWrapper-',
+    dbVersion: 1,
+    keyPath: 'id',
+    autoIncrement: true,
+    indexes: [],
+    onStoreReady: getAllFromDB,
+    onError: function(error){ throw error; }
+  });
+
+  $scope.addNote = function() {
+    var note_obj = {
+      description : $scope.noteDescription,
+      createdAt   : moment().format("MMM Do YY")
+    }
+
+    $scope.store.put(note_obj)
+    $scope.noteDescription = '';
+    getAllFromDB();
+  }
+
   $scope.deleteNote = function(index) {
     $scope.notes.splice(index, 1);
   }
